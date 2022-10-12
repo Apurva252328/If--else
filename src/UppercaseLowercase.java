8fb6cd2f1bc14fe660435ffac56ce131fd2f7f2b
Apurva patel <apurva252328@gmail.com>
2022-10-12 22:24:57 +0100

    UppercaseLowercase 2 commit

--- src/UppercaseLowercase.java
+++ src/UppercaseLowercase.java
@@ -2,13 +2,16 @@ import java.util.Scanner;
 
 public class UppercaseLowercase {
     public static void main(String[] args){
+         char a ,b;//declare local variable
         Scanner scanner = new Scanner(System.in);//creating scanner object can access by main method
         System.out.println("Enter a Alphabet :");
-        char ch = scanner.next().charAt(0);//declare local variable
+        char ch = scanner.next().charAt(0);
         if((ch>='A')&&(ch<='Z')){//conditions for uppercase
-          System.out.println("You entered a uppercase alphabet.");
+            a= Character.toLowerCase(ch);
+          System.out.println("You entered a lowercase alphabet= " +a);
         }else {
-            System.out.println("You entered a lowercase of alphabet.");
+            b= Character.toUpperCase(ch);
+            System.out.println("You entered a uppercase of alphabet= " +b);
         }
     }
 }
